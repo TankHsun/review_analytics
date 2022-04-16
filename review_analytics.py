01cdb7576f6cdf3d4c6deec82889269dec32793a
Tank Chen <a9304107@gmail.com>
2022-04-16 10:54:12 +0800

    'update_dictionary'

--- review_analytics.py
+++ review_analytics.py
@@ -10,5 +10,21 @@ with open('reviews.txt', 'r') as f:
 			print('讀取進度', len(data)/10000,'%')
 print('留言平均長度為', sum_all/len(data))
 
-
-	
+#單字記數
+wc={} #word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+while True:
+	search = input('你要查詢的單字為(o u t:離開)')
+	if search =='o u t':
+		break
+	else:
+		if search in wc:
+			print(wc[search])
+		else:
+			print('查無此單字')
